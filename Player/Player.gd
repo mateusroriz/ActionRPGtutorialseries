9bc480b876e07b142807070352a20d96d0582064
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-10-20 20:52:48 -0300

    adding attack animations for player and setting up a state machine for attack

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,13 +4,35 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+enum{
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer 
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback") #getting acess to the animations in the animation tree
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta): ## step event runs every single physics step
+	match state:
+		MOVE:
+			move_state(delta)
+		
+		ROLL:
+			pass
+			
+		ATTACK:
+			attack_state(delta)
+			
+	
+func move_state(delta): 
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") # 1, 0, -1
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -19,6 +41,7 @@ func _physics_process(delta): ## step event runs every single physics step
 	if input_vector != Vector2.ZERO: # we are moving
 		animationTree.set("parameters/Idle/blend_position", input_vector) #set the bland position for the idle 
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector) # has to be here because we have access to input vector
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -26,3 +49,13 @@ func _physics_process(delta): ## step event runs every single physics step
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity) 
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
