6e9682b91d65ccee7a41d315b62b66e80374130c
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-10-27 22:20:21 -0300

    adding Basic chase state and idle state to bat enemy

---
+++ Enemies/PlayerDetectionZone.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var player = null 
+
+func can_see_player():
+	return player != null #return true or false based on wheter or not is inside detection zone
+
+
+func _on_PlayerDetectionZone_body_entered(body):
+	player = body
+
+func _on_PlayerDetectionZone_body_exited(body):
+	player = null
