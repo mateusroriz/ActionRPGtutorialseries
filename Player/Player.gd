13276df87f84b44439baa0543599b5e41f74f5d0
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-10-31 19:57:57 -0300

    adding wander state to enemy bats

--- Player/Player.gd
+++ Player/Player.gd
@@ -23,6 +23,7 @@ onready var swordHitbox = $HitboxPivot/SwordHitbox
 onready var hurtbox = $Hurtboxes
 
 func _ready():
+	randomize()
 	stats.connect("no_health", self, "queue_free") #connecting to own queue_free
 	animationTree.active = true
 	swordHitbox.knockback_vector = roll_vector
